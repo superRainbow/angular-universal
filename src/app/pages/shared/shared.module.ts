ae5ccc5af7496fc3c15feaf0b0e2dd9f2de44d67
吳采洪 <g901612002@gmail.com>
2018-02-10 16:33:08 +0800

    為了放到 heroku 調整 package.json 架構

--- src/app/pages/shared/shared.module.ts
+++ src/app/pages/shared/shared.module.ts
@@ -3,8 +3,6 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import {BusyModule} from 'tixif-ngx-busy';
-
 import { Ng2Bs3ModalModule } from 'ng2-bs3-modal/ng2-bs3-modal';
 
 @NgModule({
@@ -13,14 +11,12 @@ import { Ng2Bs3ModalModule } from 'ng2-bs3-modal/ng2-bs3-modal';
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
-    BusyModule,
     Ng2Bs3ModalModule
   ],
   exports: [
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
-    BusyModule,
     Ng2Bs3ModalModule
   ],
   declarations: []
